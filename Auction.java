8ebda95d125daf00da0ca74318a92ec157e8d818
Matteo Dall'Olmo <matteo.dallolmo1@gmail.com>
2021-11-02 18:00:06 -0700

    working on auction class

--- Auction.java
+++ Auction.java
@@ -1,5 +1,15 @@
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Auction
 {
     private String title;
-    
+    private String image;
+    private Date startDate;
+    private Date endDate;
+    private int startPrice;
+
+    private ArrayList<Bid> arr = new ArrayList<Bid>();
+    private int views = 0;
 }
---
+++ Bid.java
@@ -0,0 +1,8 @@
+import java.sql.Date;
+
+public class Bid {
+    private String username;
+    private String walletAddress;
+    private Date bidDate;
+    private int bidPrice;
+}
